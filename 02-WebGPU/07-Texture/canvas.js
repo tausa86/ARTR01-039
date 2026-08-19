a79f4d3b035d3182ff94b6b39729bfd168f7011f
Taufik Sayyed <tausay86@gmail.com>
2026-08-20 01:18:45 +0530

    updated

--- 02-WebGPU/07-Texture/canvas.js
+++ 02-WebGPU/07-Texture/canvas.js
@@ -463,8 +463,8 @@ async function initialize() {
     bindingGroups_mvpUniform_square = createBindGroupForUniform(buffer_mvpUniform_square, 0, mvpUniformBufferSize, 0, bindGroupLayout_mvpUniform);
 
     // Load image and create texture and sampler for the square
-    texture_checkerboard = await loadTexture();
-    if(texture_checkerboard == null)
+    texture_smiley = await loadTexture("Smiley.png");
+    if(texture_smiley == null)
     {
         console.log("initialize() Loading texture for square failed\n");
         throw new Error("initialize() Loading texture for square failed\n");
@@ -481,8 +481,8 @@ async function initialize() {
     };
 
     // Create texture sampler
-    sampler_checkerboard = device.createSampler(samplerDescriptor);
-    if(sampler_checkerboard == null)
+    sampler_smiley = device.createSampler(samplerDescriptor);
+    if(sampler_smiley == null)
     {
         console.log("initialize() Creating texture sampler for square failed\n");
         throw new Error("initialize() Creating texture sampler for square failed\n");
@@ -614,31 +614,14 @@ async function initialize() {
 
 }
 
-function makeCheckImage() {
-    // Multiple by 4 as we need to specify RGBA component value for single pixel
-    let colorData = new Array(checkImageWidth * checkImageHeight * 4);
-
-    for (let heightCounter = 0; heightCounter < checkImageHeight; ++heightCounter) {
-        for (let widthCounter = 0; widthCounter < checkImageWidth; ++widthCounter) {
-            for (let componentCounter = 0; componentCounter < 4; ++componentCounter) {
-                let useWhiteColor = ((heightCounter & 0x8) ^ (widthCounter & 0x8)) === 0 || componentCounter === 3;
-                colorData[4 * heightCounter * checkImageHeight + 4 * widthCounter + componentCounter] = 255 * useWhiteColor;
-            }
-        }
-    }
-
-    checkImage = new Uint8ClampedArray(colorData);
-}
-
  // 3 UDF for texture and sampler bind group layout and bind group creation
-async function loadTexture() {
+async function loadTexture(_imageFileName) {
     // Code
-    makeCheckImage();
+    const image = new Image(); 
+    image.src = _imageFileName;
+    await image.decode(); // Wait for the image to be decoded
 
-    uint8clampedArray = new Uint8ClampedArray(checkImage);
-    const imageData = new ImageData(uint8clampedArray, checkImageWidth, checkImageHeight);
-
-    const imageBitmap = await createImageBitmap(imageData);
+    const imageBitmap = await createImageBitmap(image);
     if(imageBitmap == null)
     {
         console.log("loadTexture() Creating ImageBitmap failed\n");
@@ -1119,10 +1102,14 @@ function uninitialize() {
    // Destroy the texture and sampler for the square
    if(texture_checkerboard != null)
    {
-       texture_checkerboard.destroy();
-       texture_checkerboard = null;
+       texture_smiley.destroy();
+       texture_smiley = null;
    }
-   
+   if(sampler_smiley != null)
+   {
+       sampler_smiley.destroy();
+       sampler_smiley = null;
+   }
 
    // Step 18 Unconfigure & Destroy the context
    if(null != context)
@@ -1147,13 +1134,10 @@ function uninitialize() {
         bindingGroups_mvpUniform_square = null;
         bindingGroups_mvpUniform_triangle = null;
 
-        sampler_checkerboard = null;
+        sampler_smiley = null;
         bind_group_texture_and_sampler = null;
     }
  
-    checkImage = null;
-    uint8clampedArray = null;
-    texture_checkerboard = null;
     perspectiveProjectionMatrix = null;
     console.log("WebGPU uninitialization done successfully\n");
 }
